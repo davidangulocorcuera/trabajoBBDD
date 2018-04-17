7d37e7598076a96b3e2239d94b9d194b9a99ef84
David <davidangulocorcuera@gmail.com>
2018-04-17 09:42:06 +0200

    codigo tabulado

--- src/components/LoginRegistro/LoginRegistro.component.js
+++ src/components/LoginRegistro/LoginRegistro.component.js
@@ -1,59 +1,56 @@
 import firebase from 'firebase'
 
 export default {
-  name: 'login-registro',
-  components: {},
-  props: [],
-  data () {
-    return {
-    btLoginRegister : true,
-    sRegisterEmail: '',
-    sRegisterPassword: '',
-    sRegisterPassword2: '',
-    sLoginEmail: '',
-    sLoginPassword: '',
-    sNombre: '',
-    sGenero: '',
-  
-    }
-  },
-  computed: {
+    name: 'login-registro',
+    components: {},
+    props: [],
+    data() {
+        return {
+            btLoginRegister: true,
+            sRegisterEmail: '',
+            sRegisterPassword: '',
+            sRegisterPassword2: '',
+            sLoginEmail: '',
+            sLoginPassword: '',
+            sNombre: '',
+            sGenero: '',
 
-  },
-  mounted () {
-
-  },
-  methods: {
-    btnRegistrar1: function (event){
-      this.btLoginRegister = false
-    },
-
-    btnCancelar: function (event){
-      this.btLoginRegister = true
-    },
-  btnRegistrarse: function (event){
-      firebase.auth().createUserWithEmailAndPassword(this.sRegisterEmail,this.sRegisterPassword).then(
-        function(user){
-          alert("tu cuenta fue creada");
-        },
-        function(err){
-          console.log(err)
-          alert("error en la creacion de cuenta");
         }
-      );
     },
-    btnLogin: function (event){
+    computed: {
+    },
+    mounted() {
+    },
+    methods: {
+        btnRegistrar1: function(event) {
+            this.btLoginRegister = false
+        },
+        btnCancelar: function(event) {
+            this.btLoginRegister = true
+        },
+        btnRegistrarse: function(event) {
+            firebase.auth().createUserWithEmailAndPassword(this.sRegisterEmail, this.sRegisterPassword).then(
+                function(user) {
+                    alert("tu cuenta fue creada");
+                },
+                function(err) {
+                    console.log(err)
+                    alert("error en la creacion de cuenta");
+                }
+            );
+        },
+        btnLogin: function(event) {
 
-        firebase.auth().signInWithEmailAndPassword(this.sLoginEmail,this.sLoginPassword).then(
-          function(user){
+            firebase.auth().signInWithEmailAndPassword(this.sLoginEmail, this.sLoginPassword).then(
+                function(user) {
 
-          alert("te logeastes correctamente");
-          },
-          function(err){
-            console.log(err)
-            alert("NO TE LOGEASTE" + err);
-          }
-        );
-      }
-  }
+                    alert("te logeastes correctamente");
+                },
+                function(err) {
+                    console.log(err)
+                    alert("NO TE LOGEASTE" + err);
+                }
+            );
+        }
+    }
 }
